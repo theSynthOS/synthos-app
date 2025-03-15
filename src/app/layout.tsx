8abeb6b45b7093f2716c819bf55a5c2678833aee
cheongyeechian <cheongyeechian@gmail.com>
2025-03-15 11:08:18 +0800

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
@@ -44,5 +44,5 @@ export default function RootLayout({
         </ThirdwebProvider>
       </body>
     </html>
-  );
+  )
 }
